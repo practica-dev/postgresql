a3de5c029ea4063fd1343e90adc97ac1869c8c63
Manuel Florez <manuelflorezw@gmail.com>
2024-02-27 11:05:31 -0500

    add script

--- script.sql
+++ script.sql
@@ -1,17 +1,17 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
   user_email VARCHAR(255) PRIMARY KEY,
   user_password TEXT NOT NULL,
   user_created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   user_updated_at TIMESTAMP DEFAULT NULL
 );
 
-CREATE TABLE roles (
+CREATE TABLE IF NOT EXISTS roles (
   role_name VARCHAR(255) PRIMARY KEY,
   role_created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   role_updated_at TIMESTAMP DEFAULT NULL
 );
 
-CREATE TABLE user_role (
+CREATE TABLE IF NOT EXISTS user_role (
   user_email VARCHAR(255),
   role_name VARCHAR(255),
   user_role_created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -25,13 +25,13 @@ CREATE TABLE user_role (
 	      REFERENCES roles(role_name)
 );
 
-CREATE TABLE types_exercise (
+CREATE TABLE IF NOT EXISTS types_exercise (
   type_exercise_name VARCHAR(255) PRIMARY KEY,
   type_exercise_created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   type_exercise_updated_at TIMESTAMP DEFAULT NULL
 );
 
-CREATE TABLE exercises (
+CREATE TABLE IF NOT EXISTS exercises (
   exercise_name VARCHAR(255),
   type_exercise_name VARCHAR(255),
   exercise_name_english VARCHAR(255) NOT NULL,
@@ -44,17 +44,40 @@ CREATE TABLE exercises (
 	      REFERENCES types_exercise(type_exercise_name)
 );
 
-create extension if not exists pgcrypto;
+/* crear extension */
 
-INSERT INTO users (user_email, user_password) 
-VALUES ('manuelflorezw@gmail.com', pgp_sym_encrypt('password123', 'secret')::VARCHAR);
+CREATE extension IF NOT EXISTS pgcrypto;
 
-INSERT INTO roles (role_name) VALUES ('ADMIN');
+/* agregar datos iniciales */
 
-INSERT INTO user_role (user_email, role_name)
-VALUES ('manuelflorezw@gmail.com', 'ADMIN');
+INSERT INTO users (
+  user_email,
+  user_password
+) 
+VALUES (
+  'manuelflorezw@gmail.com',
+  pgp_sym_encrypt('password123', 'secret')::VARCHAR
+);
 
-INSERT INTO types_exercise (type_exercise_name) VALUES ('CARDIO');
+INSERT INTO roles (role_name) VALUES
+('ADMIN'),
+('USER'),
+('SELLER');
+
+INSERT INTO user_role (
+  user_email,
+  role_name)
+VALUES (
+  'manuelflorezw@gmail.com',
+  'ADMIN'
+),
+(
+  'manuelflorezw@gmail.com',
+  'USER'
+);
+
+INSERT INTO types_exercise (type_exercise_name) VALUES
+('CARDIO');
 
 INSERT INTO exercises (
   exercise_name,
